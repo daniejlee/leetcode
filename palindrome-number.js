d4574428e7ddaee8ce09a2536fb68cf035334932
Daniel Lee <daniejlee@gmail.com>
2020-11-24 23:38:43 -0800

    palindrome number

---
+++ palindrome-number.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  if (x < 0){
+    return false
+  }
+  const numString = x.toString();
+  let reverseNum = "";
+  for(let i = numString.length - 1; i >= 0; i--){
+    reverseNum += numString[i];
+  }
+  if(numString === reverseNum){
+    return true;
+  }
+  else{
+    return false;
+  }
+
+};
