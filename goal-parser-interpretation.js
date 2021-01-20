c5dd25abc73889d90b4eb6fba7868f059d031aa4
Daniel Lee <daniejlee@gmail.com>
2021-01-19 21:29:09 -0800

    regex

---
+++ goal-parser-interpretation.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function (command) {
+  let output = "";
+  let GChar = "G";
+  let oChar = /\(\)/;
+  let alChar = /\(al\)/;
+
+};
