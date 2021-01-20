5df7265842c71c45bfbcba087e7e321f80b9cd9e
Daniel Lee <daniejlee@gmail.com>
2021-01-19 21:42:31 -0800

    submit

--- goal-parser-interpretation.js
+++ goal-parser-interpretation.js
@@ -3,10 +3,8 @@
  * @return {string}
  */
 var interpret = function (command) {
-  // let GChar = "G";
   let oChar = /\(\)/g;
   let alChar = /\(al\)/g;
   let charReplace = command.replace(oChar, "o").replace(alChar, "al");
-  // let alReplace = command.replace(alChar, "al");
   return charReplace
 };
