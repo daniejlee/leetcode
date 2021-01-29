5a522dd14b89a650f849552fd55b6223d49899ea
Daniel Lee <daniejlee@gmail.com>
2021-01-28 23:39:14 -0800

    f

--- valid-anagram.js
+++ valid-anagram.js
@@ -8,5 +8,28 @@ var isAnagram = function (s, t) {
     return false
   }
 
+  if(s.length === 1){
+    return (s===t)
+  }
+
+  let sSort = s.split('').sort()
+  let tSort = t.split('').sort()
+  for(let i = 0; i< sSort.length; i++){
+    if(sSort[i] !== tSort[i]){
+      return false;
+    }
+  }
+  return true;
+
+
+  // for (i = 0; i < s.length; i++) {
+  //   if (t.includes(s[i])) {
+  //     t = t.replace(s[i], '')
+  //   }
+  //   else {
+  //     return false
+  //   }
+  // }
+  // return true;
 
 }
